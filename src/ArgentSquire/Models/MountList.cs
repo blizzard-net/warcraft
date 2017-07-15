c0bd876aaa0074ec7564644f93f1748a74321e36
Dan Jagnow <dan.jagnow@gmail.com>
2017-07-15 14:44:44 -0500

    Added support for the Mount API
    Added support for the Mount API.  Closes #9.

---
+++ src/ArgentSquire/Models/MountList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ArgentSquire
+{
+    public class MountList
+    {
+        /// <summary>
+        /// Gets or sets the list of possible mounts.
+        /// </summary>
+        [JsonProperty("mounts")]
+        public IList<Mount> Mounts { get; set; }
+    }
+}
