edd8a17676134df00c40c9a38c5fc30df4259c83
Dan Jagnow <dan.jagnow@gmail.com>
2017-09-23 19:27:08 -0500

    Updated NuGet package metadata

--- src/ArgentPonyWarcraftClient/Pack.bat
+++ src/ArgentPonyWarcraftClient/Pack.bat
@@ -1,1 +1,1 @@
-msbuild ArgentPonyWarcraftClient.csproj /t:pack /p:Configuration=Release /p:IncludeSymbols=true /p:IncludeSource=true /p:PackageVersion=1.0.0
+msbuild ArgentPonyWarcraftClient.csproj /t:pack /p:Configuration=Release /p:IncludeSymbols=true /p:IncludeSource=true /p:Version=1.0.0 /p:PackageVersion=1.0.0
