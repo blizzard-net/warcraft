979d58c07b1754d4df6f3617184926e74de925d2
Travis Boatman <TravisBoatman@users.noreply.github.com>
2018-02-15 16:24:56 -0700

    Remove the need for Enums.NET
    Turns out it was easier than I thought.

---
+++ src/ArgentPonyWarcraftClient/Utilities/EnumFieldExtentions.cs
@@ -0,0 +1,40 @@
+﻿namespace ArgentPonyWarcraftClient
+{
+    internal static class EnumFieldExtentions
+    {
+        public static string BuildQueryString(this CharacterFields fields)
+        {
+            // The Blizzard API only accepts lowercase fields.
+            string flags = fields.ToString().ToLower();
+            
+            if (flags == "all")
+            {
+                return "&fields=achievements,appearance,feed,guild,hunter pets,items,mounts,pets,pet slots," +
+                    "professions,progression,pvp,quests,reputation,statistics,stats,talents,titles,audit";
+            }
+            if (flags == "none")
+            {
+                return string.Empty;
+            }
+
+            return ($"&fields={flags}");
+        }
+
+        public static string BuildQueryString(this GuildFields fields)
+        {
+            // The Blizzard API only accepts lowercase fields.
+            string flags = fields.ToString().ToLower();
+
+            if (flags == "all")
+            {
+                return "&fields=members,achievements,news,challenge";
+            }
+            if (flags == "none")
+            {
+                return string.Empty;
+            }
+
+            return ($"&fields={flags}");
+        }
+    }
+}
