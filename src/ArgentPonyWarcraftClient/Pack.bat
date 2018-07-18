416edf1cf3baf52dce88e4c272e02c4b50c8ae42
Dan Jagnow <dan.jagnow@gmail.com>
2018-07-18 00:08:15 -0500

    Updated Pack.bat
    Updated the sample command line in Pack.bat to use dotnet pack instead of msbuild.

--- src/ArgentPonyWarcraftClient/Pack.bat
+++ src/ArgentPonyWarcraftClient/Pack.bat
@@ -1,1 +1,1 @@
-msbuild ArgentPonyWarcraftClient.csproj /t:pack /p:Configuration=Release /p:IncludeSymbols=true /p:IncludeSource=true /p:Version=1.0.0 /p:PackageVersion=1.0.0
+dotnet pack ArgentPonyWarcraftClient.csproj --configuration Release /p:Version=2.0.0 /p:PackageVersion=2.0.0
