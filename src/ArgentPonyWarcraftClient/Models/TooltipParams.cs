0811d35aca2025ede9b14ac9a5f80433eb19ba33
Dan Jagnow <dan.jagnow@gmail.com>
2018-08-25 10:43:44 -0500

    Added Azerite power properties to the TooltipParams class

--- src/ArgentPonyWarcraftClient/Models/TooltipParams.cs
+++ src/ArgentPonyWarcraftClient/Models/TooltipParams.cs
@@ -42,5 +42,35 @@ namespace ArgentPonyWarcraftClient
         /// </summary>
         [JsonProperty(PropertyName = "timewalkerLevel")]
         public int TimewalkerLevel { get; private set; }
+
+        /// <summary>
+        /// Gets the Azerite power 0.
+        /// </summary>
+        [JsonProperty(PropertyName = "azeritePower0")]
+        public int AzeritePower0 { get; private set; }
+
+        /// <summary>
+        /// Gets the Azerite power 1.
+        /// </summary>
+        [JsonProperty(PropertyName = "azeritePower1")]
+        public int AzeritePower1 { get; private set; }
+
+        /// <summary>
+        /// Gets the Azerite power 2.
+        /// </summary>
+        [JsonProperty(PropertyName = "azeritePower2")]
+        public int AzeritePower2 { get; private set; }
+
+        /// <summary>
+        /// Gets the Azerite power 3.
+        /// </summary>
+        [JsonProperty(PropertyName = "azeritePower3")]
+        public int AzeritePower3 { get; private set; }
+
+        /// <summary>
+        /// Gets the Azerite power level.
+        /// </summary>
+        [JsonProperty(PropertyName = "azeritePowerLevel")]
+        public int AzeritePowerLevel { get; private set; }
     }
 }
