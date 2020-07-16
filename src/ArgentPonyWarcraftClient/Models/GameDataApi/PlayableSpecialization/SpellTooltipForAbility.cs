6b10f1a014689e6b4be1da6d7898ee9aea38ea7c
Dan Jagnow <dan.jagnow@gmail.com>
2020-07-15 23:06:25 -0500

    Added support for the Character Specializations API
    Added support for the Character Specializations API.  Added an ICharacterSpecializationsApi interface to describe the capabilities of the API.  The IWarcraftClient interface implements this interface via the IProfileApi interface.

---
+++ src/ArgentPonyWarcraftClient/Models/GameDataApi/PlayableSpecialization/SpellTooltipForAbility.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace ArgentPonyWarcraftClient
+{
+    /// <summary>
+    /// A spell tooltip.
+    /// </summary>
+    public class SpellTooltipForAbility
+    {
+        /// <summary>
+        /// Gets a reference to the spell.
+        /// </summary>
+        [JsonProperty("spell", NullValueHandling = NullValueHandling.Ignore)]
+        public SpellReference Spell { get; set; }
+
+        /// <summary>
+        /// Gets the description of the spell.
+        /// </summary>
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets the cast time of the spell.
+        /// </summary>
+        [JsonProperty("cast_time")]
+        public string CastTime { get; set; }
+
+        /// <summary>
+        /// Gets the cooldown of the spell.
+        /// </summary>
+        [JsonProperty("cooldown", NullValueHandling = NullValueHandling.Ignore)]
+        public string Cooldown { get; set; }
+    }
+}
