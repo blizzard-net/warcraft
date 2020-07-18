3b27863d442482f304ab7b138e9b1ef150fbe800
Billy Wolfington <willwolfram18@gmail.com>
2020-07-18 09:32:34 -0700

    Added new project and test project
    Added new extensions project for handling the dependency injection, as well as a test project for it.

---
+++ tests/ArgentPonyWarcraftClient.Extensions.DependencyInjection.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace ArgentPonyWarcraftClient.Extensions.DependencyInjection.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
