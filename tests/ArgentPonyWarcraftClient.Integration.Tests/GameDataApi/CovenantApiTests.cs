7163640b4d8e5a2a255b70a756e0978c4e84cb35
willwolfram18 <willwolfram18@gmail.com>
2020-12-03 11:22:21 -0800

    Covenant API integration tests

--- tests/ArgentPonyWarcraftClient.Integration.Tests/GameDataApi/CovenantApiTests.cs
+++ tests/ArgentPonyWarcraftClient.Integration.Tests/GameDataApi/CovenantApiTests.cs
@@ -6,53 +6,93 @@ namespace ArgentPonyWarcraftClient.Integration.Tests.GameDataApi
     public class CovenantApiTests
     {
         [ResilientFact]
-        public Task When_Getting_A_Conduit_By_Id_Then_Successful_Result_With_Expected_Content_Is_Returned()
+        public async Task When_Getting_A_Conduit_By_Id_Then_Successful_Result_With_Expected_Content_Is_Returned()
         {
             ICovenantApi client = ClientFactory.BuildClient();
 
-            Assert.True(false, "TODO");
+            RequestResult<Conduit> result = await client.GetConduitAsync(17, "static-us");
 
-            return Task.CompletedTask;
+            Assert.NotNull(result);
+            Assert.True(result.Success);
+            Assert.Null(result.Error);
+            Assert.NotNull(result.Value);
+
+            await AssertResultMatchesBlizzardResponseAsync(
+                result,
+                "https://us.api.blizzard.com/data/wow/covenant/conduit/17?namespace=static-us&locale=en_US"
+            );
         }
 
         [ResilientFact]
-        public Task When_Getting_A_Covenant_By_Id_Then_Successful_Result_With_Expected_Content_Is_Returned()
+        public async Task When_Getting_A_Covenant_By_Id_Then_Successful_Result_With_Expected_Content_Is_Returned()
         {
             ICovenantApi client = ClientFactory.BuildClient();
 
-            Assert.True(false, "TODO");
+            RequestResult<Covenant> result = await client.GetCovenantAsync(3, "static-us");
 
-            return Task.CompletedTask;
+            Assert.NotNull(result);
+            Assert.True(result.Success);
+            Assert.Null(result.Error);
+            Assert.NotNull(result.Value);
+
+            await AssertResultMatchesBlizzardResponseAsync(
+                result,
+                "https://us.api.blizzard.com/data/wow/covenant/3?namespace=static-us&locale=en_US"
+            );
         }
 
         [ResilientFact]
-        public Task When_Getting_A_Soulbind_By_Id_Then_Successful_Result_With_Expected_Content_Is_Returned()
+        public async Task When_Getting_A_Soulbind_By_Id_Then_Successful_Result_With_Expected_Content_Is_Returned()
         {
             ICovenantApi client = ClientFactory.BuildClient();
 
-            Assert.True(false, "TODO");
+            RequestResult<Soulbind> result = await client.GetSoulbindAsync(9, "static-us");
 
-            return Task.CompletedTask;
+            Assert.NotNull(result);
+            Assert.True(result.Success);
+            Assert.Null(result.Error);
+            Assert.NotNull(result.Value);
+
+            await AssertResultMatchesBlizzardResponseAsync(
+                result,
+                "https://us.api.blizzard.com/data/wow/covenant/soulbind/9?namespace=static-us&locale=en_US"
+            );
         }
 
         [ResilientFact]
-        public Task When_Getting_Conduits_Index_Then_Successful_Result_With_Expected_Content_Is_Returned()
+        public async Task When_Getting_Conduits_Index_Then_Successful_Result_With_Expected_Content_Is_Returned()
         {
             ICovenantApi client = ClientFactory.BuildClient();
 
-            Assert.True(false, "TODO");
+            RequestResult<ConduitsIndex> result = await client.GetConduitsIndexAsync("static-us");
 
-            return Task.CompletedTask;
+            Assert.NotNull(result);
+            Assert.True(result.Success);
+            Assert.Null(result.Error);
+            Assert.NotNull(result.Value);
+
+            await AssertResultMatchesBlizzardResponseAsync(
+                result,
+                "https://us.api.blizzard.com/data/wow/covenant/conduit/index?namespace=static-us&locale=en_US"
+            );
         }
 
         [ResilientFact]
-        public Task When_Getting_Covenant_Media_Then_Successful_Result_With_Expected_Content_Is_Returned()
+        public async Task When_Getting_Covenant_Media_Then_Successful_Result_With_Expected_Content_Is_Returned()
         {
             ICovenantApi client = ClientFactory.BuildClient();
 
-            Assert.True(false, "TODO");
+            RequestResult<CovenantMedia> result = await client.GetCovenantMediaAsync(3, "static-us");
 
-            return Task.CompletedTask;
+            Assert.NotNull(result);
+            Assert.True(result.Success);
+            Assert.Null(result.Error);
+            Assert.NotNull(result.Value);
+
+            await AssertResultMatchesBlizzardResponseAsync(
+                result,
+                "https://us.api.blizzard.com/data/wow/media/covenant/3?namespace=static-us&locale=en_US"
+            );
         }
 
         [ResilientFact]
@@ -74,13 +114,21 @@ namespace ArgentPonyWarcraftClient.Integration.Tests.GameDataApi
         }
 
         [ResilientFact]
-        public Task When_Getting_Soulbinds_Index_Then_Successful_Result_With_Expected_Content_Is_Returned()
+        public async Task When_Getting_Soulbinds_Index_Then_Successful_Result_With_Expected_Content_Is_Returned()
         {
             ICovenantApi client = ClientFactory.BuildClient();
 
-            Assert.True(false, "TODO");
+            RequestResult<SoulbindsIndex> result = await client.GetSoulbindsIndexAsync("static-us");
 
-            return Task.CompletedTask;
+            Assert.NotNull(result);
+            Assert.True(result.Success);
+            Assert.Null(result.Error);
+            Assert.NotNull(result.Value);
+
+            await AssertResultMatchesBlizzardResponseAsync(
+                result,
+                "https://us.api.blizzard.com/data/wow/covenant/soulbind/index?namespace=static-us&locale=en_US"
+            );
         }
 
         private async Task AssertResultMatchesBlizzardResponseAsync<T>(RequestResult<T> actualValue, string blizzardRequestUri)
