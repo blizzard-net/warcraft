304855c47a98bdca8d86d3558365d338cae1c379
willwolfram18 <willwolfram18@gmail.com>
2020-12-03 10:22:10 -0800

    Fixed up null value handling on json checks
    Needed to include some code that prunes null values from the expected and actual JSON values because some API responses from Blizzard have a mix of null value vs. missing property responses. An example of this is spell tooltips, where some properties like power cost are omitted for some tooltips, while other responses have the field set to null.

--- tests/ArgentPonyWarcraftClient.Tests/RequestResultExtensions.cs
+++ tests/ArgentPonyWarcraftClient.Tests/RequestResultExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Linq;
+using System.Text.Json;
 using FluentAssertions.Json;
 using Newtonsoft.Json.Linq;
 
@@ -15,13 +16,28 @@ namespace ArgentPonyWarcraftClient.Tests
         /// <param name="expectedJsonContent">The expected JSON content.</param>
         public static void ShouldMatchJsonContent<T>(this RequestResult<T> requestResult, string expectedJsonContent)
         {
-            var actualJsonValue = JObject.Parse(JsonSerializer.Serialize(requestResult.Value, new JsonSerializerOptions
-            {
-                IgnoreNullValues = true
-            }));
-            var expectedJsonValue = JObject.Parse(expectedJsonContent);
+            // Compare values as JObjects, removing null values on both actual and result. This
+            // addresses an issues where objects like spell tooltips sometimes use a null value and sometimes
+            // exclude a property, e.g. { "cast_time": "Passive" } vs. { "cast_time": "Instant", "power_cost": null }
+            var actualJsonValue = JObject.Parse(JsonSerializer.Serialize(requestResult.Value)).RemoveNullProperties();
+            var expectedJsonValue = JObject.Parse(expectedJsonContent).RemoveNullProperties();
 
             actualJsonValue.Should().BeEquivalentTo(expectedJsonValue);
         }
+
+        private static JObject RemoveNullProperties(this JObject jObject)
+        {
+            // Code inspired by: https://stackoverflow.com/a/60182337
+            while (jObject.Descendants().Any(IsPropertyAndIsNullValue))
+            {
+                foreach (JToken token in jObject.Descendants().Where(IsPropertyAndIsNullValue).ToArray())
+                {
+                    token.Remove();
+                }
+            }
+            return jObject;
+        }
+
+        private static bool IsPropertyAndIsNullValue(JToken jt) => jt.Type == JTokenType.Property && (jt.Values().All(a => a.Type == JTokenType.Null));
     }
 }
