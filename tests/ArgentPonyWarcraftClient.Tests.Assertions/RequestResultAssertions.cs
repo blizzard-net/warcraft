c51843c79bb88260b1e6dffb077df83f692e0817
William Wolfram <willwolfram18@gmail.com>
2020-12-10 22:59:34 -0800

    Referenced FluentAssertions.Json
    Needed to add FluentAssertions.Json to ensure error messages were more explicit about missing properties.

--- tests/ArgentPonyWarcraftClient.Tests.Assertions/RequestResultAssertions.cs
+++ tests/ArgentPonyWarcraftClient.Tests.Assertions/RequestResultAssertions.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Text.Json;
 using FluentAssertions;
 using FluentAssertions.Execution;
+using FluentAssertions.Json;
 using FluentAssertions.Primitives;
 using Newtonsoft.Json.Linq;
 
