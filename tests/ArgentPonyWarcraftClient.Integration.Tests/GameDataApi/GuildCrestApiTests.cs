1f34f4d88ca247e82bf7afe8648c8c813b8a5ba3
Dan Jagnow <dan.jagnow@gmail.com>
2021-07-14 17:48:37 -0500

    Improved GuildCrestApiTests
    Improved GuildCrestApiTests by using the test utilities for deep comparison.

--- tests/ArgentPonyWarcraftClient.Integration.Tests/GameDataApi/GuildCrestApiTests.cs
+++ tests/ArgentPonyWarcraftClient.Integration.Tests/GameDataApi/GuildCrestApiTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
-using Xunit;
+using ArgentPonyWarcraftClient.Integration.Tests.TestUtilities;
+using ArgentPonyWarcraftClient.Tests.Assertions;
 
 namespace ArgentPonyWarcraftClient.Integration.Tests.GameDataApi
 {
@@ -9,24 +10,33 @@ namespace ArgentPonyWarcraftClient.Integration.Tests.GameDataApi
         public async Task GetGuildCrestComponentsIndexAsync_Gets_GuildCrestComponentsIndex()
         {
             IGuildCrestApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<GuildCrestComponentsIndex> result = await warcraftClient.GetGuildCrestComponentsIndexAsync("static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/guild-crest/index?namespace=static-us&locale=en_US");
         }
 
         [ResilientFact]
         public async Task GetGuildCrestBorderMediaAsync_Gets_GuildCrestBorderMedia()
         {
             IGuildCrestApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<GuildCrestBorderMedia> result = await warcraftClient.GetGuildCrestBorderMediaAsync(0, "static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/media/guild-crest/border/0?namespace=static-us&locale=en_US");
         }
 
         [ResilientFact]
         public async Task GetGuildCrestEmblemMediaAsync_Gets_GuildCrestEmblemMedia()
         {
             IGuildCrestApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<GuildCrestEmblemMedia> result = await warcraftClient.GetGuildCrestEmblemMediaAsync(0, "static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/media/guild-crest/emblem/0?namespace=static-us&locale=en_US");
         }
     }
 }
