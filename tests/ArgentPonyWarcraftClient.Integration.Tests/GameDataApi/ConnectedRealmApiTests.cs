22413ae6553736f4bcf2ff17db8dfee037cf53c4
Dan Jagnow <dan.jagnow@gmail.com>
2021-07-14 17:24:50 -0500

    Improved ConnectedRealmApiTests
    Improved ConnectedRealmApiTests by using the test utilities for deep comparison.

--- tests/ArgentPonyWarcraftClient.Integration.Tests/GameDataApi/ConnectedRealmApiTests.cs
+++ tests/ArgentPonyWarcraftClient.Integration.Tests/GameDataApi/ConnectedRealmApiTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
-using Xunit;
+using ArgentPonyWarcraftClient.Integration.Tests.TestUtilities;
+using ArgentPonyWarcraftClient.Tests.Assertions;
 
 namespace ArgentPonyWarcraftClient.Integration.Tests.GameDataApi
 {
@@ -9,16 +10,22 @@ namespace ArgentPonyWarcraftClient.Integration.Tests.GameDataApi
         public async Task GetPlayableRacesIndexAsync_Gets_PlayableRacesIndex()
         {
             IConnectedRealmApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<ConnectedRealmsIndex> result = await warcraftClient.GetConnectedRealmsIndexAsync("dynamic-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/connected-realm/index?namespace=dynamic-us&locale=en_US");
         }
 
         [ResilientFact]
         public async Task GetRealmAsync_Gets_Realm()
         {
             IConnectedRealmApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<ConnectedRealm> result = await warcraftClient.GetConnectedRealmAsync(11, "dynamic-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/connected-realm/11?namespace=dynamic-us&locale=en_US");
         }
     }
 }
