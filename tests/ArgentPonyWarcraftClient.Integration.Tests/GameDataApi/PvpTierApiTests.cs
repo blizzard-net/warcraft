888d4679d8f9f64ff1590fec2a3b31fe260287c0
Dan Jagnow <dan.jagnow@gmail.com>
2021-07-19 18:40:56 -0500

    Improved PvpTierApiTests
    Improved PvpTierApiTests by using the test utilities for deep comparison.

--- tests/ArgentPonyWarcraftClient.Integration.Tests/GameDataApi/PvpTierApiTests.cs
+++ tests/ArgentPonyWarcraftClient.Integration.Tests/GameDataApi/PvpTierApiTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
-using Xunit;
+using ArgentPonyWarcraftClient.Integration.Tests.TestUtilities;
+using ArgentPonyWarcraftClient.Tests.Assertions;
 
 namespace ArgentPonyWarcraftClient.Integration.Tests.GameDataApi
 {
@@ -9,24 +10,33 @@ namespace ArgentPonyWarcraftClient.Integration.Tests.GameDataApi
         public async Task GetPvpTiersIndexAsync_Gets_PvpTiersIndex()
         {
             IPvpTierApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<PvpTiersIndex> result = await warcraftClient.GetPvpTiersIndexAsync("static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/pvp-tier/index?namespace=static-us&locale=en_US");
         }
 
         [ResilientFact]
         public async Task GetPvpTierAsync_Gets_PvpTier()
         {
             IPvpTierApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<PvpTier> result = await warcraftClient.GetPvpTierAsync(1, "static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/pvp-tier/1?namespace=static-us&locale=en_US");
         }
 
         [ResilientFact]
         public async Task GetPvpTierMediaAsync_Gets_PvpTierMedia()
         {
             IPvpTierApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<PvpTierMedia> result = await warcraftClient.GetPvpTierMediaAsync(1, "static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/media/pvp-tier/1?namespace=static-us&locale=en_US");
         }
     }
 }
