084762f4563ec0d0336b0399cd15cdaa86db1f2c
Dan Jagnow <dan.jagnow@gmail.com>
2021-07-19 18:43:54 -0500

    Improved QuestApiTests
    Improved QuestApiTests by using the test utilities for deep comparison.

--- tests/ArgentPonyWarcraftClient.Integration.Tests/GameDataApi/QuestApiTests.cs
+++ tests/ArgentPonyWarcraftClient.Integration.Tests/GameDataApi/QuestApiTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
-using Xunit;
+using ArgentPonyWarcraftClient.Integration.Tests.TestUtilities;
+using ArgentPonyWarcraftClient.Tests.Assertions;
 
 namespace ArgentPonyWarcraftClient.Integration.Tests.GameDataApi
 {
@@ -9,64 +10,88 @@ namespace ArgentPonyWarcraftClient.Integration.Tests.GameDataApi
         public async Task GetQuestsIndexAsync_Gets_QuestsIndex()
         {
             IQuestApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<QuestsIndex> result = await warcraftClient.GetQuestsIndexAsync("static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/quest/index?namespace=static-us&locale=en_US");
         }
 
         [ResilientFact]
         public async Task GetQuestAsync_Gets_Quest()
         {
             IQuestApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<Quest> result = await warcraftClient.GetQuestAsync(2, "static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/quest/2?namespace=static-us&locale=en_US");
         }
 
         [ResilientFact]
         public async Task GetQuestCategoriesIndexAsync_Gets_QuestCategoriesIndex()
         {
             IQuestApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<QuestCategoriesIndex> result = await warcraftClient.GetQuestCategoriesIndexAsync("static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/quest/category/index?namespace=static-us&locale=en_US");
         }
 
         [ResilientFact]
         public async Task GetQuestCategoryAsync_Gets_QuestCategory()
         {
             IQuestApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<QuestCategory> result = await warcraftClient.GetQuestCategoryAsync(1, "static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/quest/category/1?namespace=static-us&locale=en_US");
         }
 
         [ResilientFact]
         public async Task GetQuestAreasIndexAsync_Gets_QuestAreasIndex()
         {
             IQuestApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<QuestAreasIndex> result = await warcraftClient.GetQuestAreasIndexAsync("static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/quest/area/index?namespace=static-us&locale=en_US");
         }
 
         [ResilientFact]
         public async Task GetQuestAreaAsync_Gets_QuestArea()
         {
             IQuestApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<QuestArea> result = await warcraftClient.GetQuestAreaAsync(1, "static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/quest/area/1?namespace=static-us&locale=en_US");
         }
 
         [ResilientFact]
         public async Task GetQuestTypesIndexAsync_Gets_QuestTypesIndex()
         {
             IQuestApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<QuestTypesIndex> result = await warcraftClient.GetQuestTypesIndexAsync("static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/quest/type/index?namespace=static-us&locale=en_US");
         }
 
         [ResilientFact]
         public async Task GetQuestTypeAsync_Gets_QuestType()
         {
             IQuestApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<QuestType> result = await warcraftClient.GetQuestTypeAsync(1, "static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/quest/type/1?namespace=static-us&locale=en_US");
         }
     }
 }
