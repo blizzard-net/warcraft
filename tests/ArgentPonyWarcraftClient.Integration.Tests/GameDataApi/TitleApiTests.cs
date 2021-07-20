9349fb6c64f4fb0989cc0df9b433bf5d29d1b7fb
Dan Jagnow <dan.jagnow@gmail.com>
2021-07-19 19:33:52 -0500

    Improved TitleApiTests
    Improved TitleApiTests by using the test utilities for deep comparison.

--- tests/ArgentPonyWarcraftClient.Integration.Tests/GameDataApi/TitleApiTests.cs
+++ tests/ArgentPonyWarcraftClient.Integration.Tests/GameDataApi/TitleApiTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
-using Xunit;
+using ArgentPonyWarcraftClient.Integration.Tests.TestUtilities;
+using ArgentPonyWarcraftClient.Tests.Assertions;
 
 namespace ArgentPonyWarcraftClient.Integration.Tests.GameDataApi
 {
@@ -9,16 +10,22 @@ namespace ArgentPonyWarcraftClient.Integration.Tests.GameDataApi
         public async Task GetTitlesIndexAsync_Gets_TitlesIndex()
         {
             ITitleApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<TitlesIndex> result = await warcraftClient.GetTitlesIndexAsync("static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                            .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/title/index?namespace=static-us&locale=en_US");
         }
 
         [ResilientFact]
         public async Task GetTitleAsync_Gets_Title()
         {
             ITitleApi warcraftClient = ClientFactory.BuildClient();
+
             RequestResult<Title> result = await warcraftClient.GetTitleAsync(1, "static-us");
-            Assert.NotNull(result.Value);
+
+            await result.Should().BeSuccessfulRequest()
+                            .BeEquivalentToBlizzardResponseAsync("https://us.api.blizzard.com/data/wow/title/1?namespace=static-us&locale=en_US");
         }
     }
 }
