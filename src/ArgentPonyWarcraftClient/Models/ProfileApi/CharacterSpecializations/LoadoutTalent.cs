c41dc7a505392dad739e661f9f2880b0dfb7378d
Hüseyin Uslu <huseyin@int6ware.com>
2023-04-06 15:00:24 +0300

    feat: added loadout field to Character Specializations API response.

---
+++ src/ArgentPonyWarcraftClient/Models/ProfileApi/CharacterSpecializations/LoadoutTalent.cs
@@ -0,0 +1,25 @@
+﻿namespace ArgentPonyWarcraftClient;
+
+/// <summary>
+/// Represents a talent entry in specialization loadouts.
+/// </summary>
+public record LoadoutTalent
+{
+    /// <summary>
+    /// Gets the ID of the loadout talent.
+    /// </summary>
+    [JsonPropertyName("id")]
+    public long Id { get; init; }
+
+    /// <summary>
+    /// Gets the rank of the loadout talent.
+    /// </summary>
+    [JsonPropertyName("rank")]
+    public long Rank { get; init; }
+
+    /// <summary>
+    /// Gets the tooltip for the loadout talent.
+    /// </summary>
+    [JsonPropertyName("tooltip")]
+    public TalentNodeTooltip Tooltip { get; init; }
+}
---
+++ src/ArgentPonyWarcraftClient/Models/ProfileApi/CharacterSpecializations/SpecializationLoadout.cs
@@ -0,0 +1,31 @@
+﻿namespace ArgentPonyWarcraftClient;
+
+/// <summary>
+/// Loadout for specialization
+/// </summary>
+public record SpecializationLoadout
+{
+    /// <summary>
+    /// Is the loadout currently active?
+    /// </summary>
+    [JsonPropertyName("is_active")]
+    public bool IsActive { get; init; }
+
+    /// <summary>
+    /// Code for the load out.
+    /// </summary>
+    [JsonPropertyName("talent_loadout_code")]
+    public string TalentLoadoutCode { get; init; }
+
+    /// <summary>
+    /// Gets the selected class talents.
+    /// </summary>
+    [JsonPropertyName("selected_class_talents")]
+    public LoadoutTalent[] SelectedClassTalents { get; init; }
+
+    /// <summary>
+    /// Gets the selected spec talents.
+    /// </summary>
+    [JsonPropertyName("selected_spec_talents")]
+    public LoadoutTalent[] SelectedSpecTalents { get; init; }
+}
